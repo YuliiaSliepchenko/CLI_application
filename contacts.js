8f5ccd802ef678ca19a28efa8952ac8ff7cdb38a
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-06-21 21:27:52 +0300

    fix_null_table

--- contacts.js
+++ contacts.js
@@ -4,22 +4,30 @@ const path = require("path");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const updateContacts = async (contacts) =>
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
+
 async function getContactById(contactId) {
   const contacts = await listContacts();
   const result = contacts.find((contact) => contact.id === contactId);
   return result || null;
 }
-async function addContact(id, name, email, phone) {
+
+async function removeContact(contactId) {
   const contacts = await listContacts();
-  const doubleContact = contacts.find((el) => el.phone === phone);
-  if (doubleContact) throw Error("This contact already exists");
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
     name,
@@ -30,19 +38,10 @@ async function addContact(id, name, email, phone) {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
-async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const contactIndex = contacts.findIndex((el) => el.id === contactId);
-  if (contactIndex === -1) throw Error("No such contact");
-  const remove = contacts[contactIndex];
-  contacts.splice(contactIndex, 1);
-  await updateContacts(contacts);
-  return remove;
-}
 
 module.exports = {
   listContacts,
   getContactById,
-  addContact,
   removeContact,
+  addContact,
 };
--- index.js
+++ index.js
@@ -13,27 +13,23 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-async function invokeAction({ action, id, name, phone, email }) {
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      console.log(allContacts);
-      break;
+      return console.table(allContacts);
     case "get":
-      const contact = await contacts.getContactById(id);
-      console.log(contact);
-      break;
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
     case "add":
-      const newContact = await contacts.addContact(id, name, email, phone);
-      console.log(newContact);
-      break;
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
     case "remove":
       const removeContact = await contacts.removeContact(id);
-      console.log(removeContact);
-      break;
-
+      return console.log(removeContact);
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
+
 invokeAction(argv);
